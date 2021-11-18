cab018305d4e3c6c74d90957349ffdff2b498334
estrellafel <felixestrella7@gmail.com>
2021-11-18 15:29:28 -0600

    added functions

--- assign3/javascript.js
+++ assign3/javascript.js
@@ -24,6 +24,97 @@ function eligibility(age, state, filed, dependents) {
     return check_age && check_state && filed && check_dependents;
 }
 
+function oddlyEven(data) {
+    if (typeof data == 'string') {
+        return decode(data);
+    } else if (typeof data == 'number') {
+        return encode(data);
+    } else {
+        return undefined;
+    }
+}
+
+function encode(data) {
+    data = data.toString().split('');
+    let retVal = '';
+    let seperator = ':';
+
+    for(let i = 0; i < data.length; i++) {
+        if (i == data.length - 1) {
+            seperator = '';
+        }
+        if (data[i] == 0) {
+            retVal = retVal.concat('*',seperator);
+        } else if (data[i] == 1) {
+            retVal = retVal.concat('#',seperator);
+        } else if (data[i] == 2) {
+            retVal = retVal.concat('**',seperator);
+        } else if (data[i] == 3) {
+            retVal = retVal.concat('##',seperator);
+        } else if (data[i] == 4) {
+            retVal = retVal.concat('***',seperator);
+        } else if (data[i] == 5) {
+            retVal = retVal.concat('###',seperator);
+        } else if (data[i] == 6) {
+            retVal = retVal.concat('****',seperator);
+        } else if (data[i] == 7) {
+            retVal = retVal.concat('####',seperator);
+        } else if (data[i] == 8) {
+            retVal = retVal.concat('*****',seperator);
+        } else if (data[i] == 9) {
+            retVal = retVal.concat('#####',seperator);
+        } else {
+            return undefined;
+        }
+    }
+
+    return retVal;
+}
+
+function decode(data) {
+    data = data.toString().split(':');
+    let retVal = '';
+    let seperator = '';
+
+    for(let i = 0; i < data.length; i++) {
+        if (data[i] == '*') {
+            retVal = retVal.concat('0',seperator);
+        } else if (data[i] == '#') {
+            retVal = retVal.concat('1',seperator);
+        } else if (data[i] == '**') {
+            retVal = retVal.concat('2',seperator);
+        } else if (data[i] == '##') {
+            retVal = retVal.concat('3',seperator);
+        } else if (data[i] == '***') {
+            retVal = retVal.concat('4',seperator);
+        } else if (data[i] == '###') {
+            retVal = retVal.concat('5',seperator);
+        } else if (data[i] == '****') {
+            retVal = retVal.concat('6',seperator);
+        } else if (data[i] == '####') {
+            retVal = retVal.concat('7',seperator);
+        } else if (data[i] == '*****') {
+            retVal = retVal.concat('8',seperator);
+        } else if (data[i] == '#####') {
+            retVal = retVal.concat('9',seperator);
+        } else {
+            return undefined;
+        }
+    }
+    return Number(retVal);
+}
+
+function allAnagrams(words) {
+    
+}
+
 // console.log(eligibility( 35, 'CA', true, 3));
 // console.log(eligibility( 61, 'wi', true, 3));
 // console.log(eligibility( 38, 'WI', false ));
+
+// console.log(oddlyEven( 18 ));
+// console.log(oddlyEven( -3 ));
+// console.log(oddlyEven( 183 ));
+// console.log(oddlyEven( '#:*****:##' ));
+// console.log(oddlyEven( 'CS' ));
+// console.log(oddlyEven( oddlyEven(318) ));
