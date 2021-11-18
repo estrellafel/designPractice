fe065d33994bb1b3ad118a72866c82c451c34cdb
estrellafel <felixestrella7@gmail.com>
2021-11-18 15:29:15 -0600

    first two functions done

--- assign3/javascript.js
+++ assign3/javascript.js
@@ -1,1 +1,29 @@
 // Felix Estrella
+
+function eligibility(age, state, filed, dependents) {
+    let check_age;
+    if (age < 60) {
+        check_age = false;
+    } else {
+        check_age = true;
+    }
+
+    let check_state;
+    if (state.toUpperCase() == 'IA' || state.toUpperCase() == 'WI') {
+        check_state = true;
+    } else {
+        check_state = false;
+    }
+
+    let check_dependents;
+    if (dependents >= 1 && dependents <= 5) {
+        check_dependents = true;
+    } else {
+        check_dependents = false;
+    }
+    return check_age && check_state && filed && check_dependents;
+}
+
+// console.log(eligibility( 35, 'CA', true, 3));
+// console.log(eligibility( 61, 'wi', true, 3));
+// console.log(eligibility( 38, 'WI', false ));
